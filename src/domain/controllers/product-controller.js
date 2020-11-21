eaa4e7cec310892ea7a411fe217822924298c8af
Nicholas-ar <njaf@ic.ufal.br>
2020-11-20 22:51:52 -0300

    feat: finished retrieveProduct documentation and tests

--- src/domain/controllers/product-controller.js
+++ src/domain/controllers/product-controller.js
@@ -9,10 +9,24 @@ export class ProductController {
   constructor(repository) {
     this.repository = repository;
   }
+
+  async createProduct(httpRequest) {
+    try {
+    } catch (error) {
+      return HTTP_SERVER_ERROR_500(error);
+    }
+  }
+
+  /**
+   * Receives an HttpRequest containing a valid cpf field in the body
+   * @param httpRequest
+   * @returns {Promise<Object>} - A 400 http response will be returned if no matches are found in the database.
+   *                            - A 500 http response will be returned if an error is thrown during the process.
+   *                            - A 200 http response will be returned otherwise, containing the product info in the body.
+   */
   async retrieveProduct(httpRequest) {
     try {
       const product = await this.repository.getByName(httpRequest.body);
-      const all = await this.repository.getAll();
       if (!product)
         return HTTP_BAD_REQUEST_400({
           message: 'No products with this name found',
