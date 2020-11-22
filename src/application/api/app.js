600e6f0f7171806291eec609954b3b7ea17658c8
Rodrigo Vaz <rodrigodp2014@gmail.com>
2020-11-22 17:21:46 -0300

    feat: add aws s3 controller

--- src/application/api/app.js
+++ src/application/api/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import router from './routes';
+import awsRouter from '../../components/aws/routes/routes-aws';
+import errorHandler from '../../middlewares/error-handler';
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/v1', router);
+app.use('/api/v1', awsRouter);
+app.use(errorHandler);
 
 export default app;
--- src/application/api/start.js
+++ src/application/api/start.js
@@ -1,5 +1,23 @@
 import app from './app';
 
+require('dotenv').config();
+
+if (!process.env.TOKEN) {
+  throw new Error('TOKEN environment variable must be defined');
+}
+if (!process.env.AMAZON_SECRET_KEY) {
+  throw new Error('AMAZON_SECRET_KEY environment variable must be defined');
+}
+if (!process.env.BUCKET) {
+  throw new Error('BUCKET environment variable must be defined');
+}
+if (!process.env.REGION) {
+  throw new Error('REGION environment variable must be defined');
+}
+if (!process.env.KEY) {
+  throw new Error('KEY environment variable must be defined');
+}
+
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
