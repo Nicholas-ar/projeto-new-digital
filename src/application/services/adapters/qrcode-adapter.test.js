fe996b4e6c64c1bba9e2a0cd6403770f04d66435
Nicholas-ar <njaf@ic.ufal.br>
2020-11-24 18:21:42 -0300

    chore: changed variable names

--- src/application/services/adapters/qrcode-adapter.test.js
+++ src/application/services/adapters/qrcode-adapter.test.js
@@ -4,9 +4,9 @@ import { toDataURL } from 'qrcode';
 describe('qrCodeAdapter', () => {
   it('Must return the expected string given a url parameter', async () => {
     const qrAdapter = new qrCodeAdapter();
-    const testString = 'This is a test';
-    const qrcode = await qrAdapter.generateQRCode(testString);
-    const temp = await toDataURL(testString);
-    expect(qrcode).toEqual(temp);
+    const mockString = 'This is a test';
+    const qrcode = await qrAdapter.generateQRCode(mockString);
+    const expectedQRCode = await toDataURL(mockString);
+    expect(qrcode).toEqual(expectedQRCode);
   });
 });
