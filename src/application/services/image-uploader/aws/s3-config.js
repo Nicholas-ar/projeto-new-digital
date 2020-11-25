baeb7f800180853a5088530e8e721481ed48c786
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-11-25 00:01:18 -0300

    refactor: changed aws variable names

--- src/application/services/image-uploader/aws/s3-config.js
+++ src/application/services/image-uploader/aws/s3-config.js
@@ -6,6 +6,6 @@ require('dotenv').config();
 export const s3Config = new AWS.S3({
   signatureVersion: 'v4',
   region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_TOKEN,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
