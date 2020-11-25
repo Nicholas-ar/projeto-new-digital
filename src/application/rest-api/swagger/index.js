f01e4777f2a380f308ca8339364b6f8e254662e3
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-11-25 12:18:11 -0300

    feat: added swagger ui signin and signup docs

--- src/application/rest-api/swagger/index.js
+++ src/application/rest-api/swagger/index.js
@@ -1,8 +1,45 @@
+import { badRequest, unauthorized, notFound, serverError } from './components';
+import { signinPath } from './paths/signin-path';
+import { signupPath } from './paths/signup-path';
+import { accountSchema } from './schemas/accountSchema';
+import { errorSchema } from './schemas/error-schema';
+import { signinParamsSchema } from './schemas/signin-params-schema';
+import { signupParamsSchema } from './schemas/signupSchema';
+
 export default {
-    openapi: '3.0.0',
-    info: {
-        title: 'QRoBuy backend',
-        description: 'Backend para a aplicação QRobuy',
-        version: '1.0.0',
-    }
-}
+  openapi: '3.0.0',
+  info: {
+    title: 'QRoBuy backend',
+    description: 'Backend para a aplicação QRobuy',
+    version: '1.0.0',
+  },
+  servers: [
+    {
+      url: '/api/v1',
+    },
+  ],
+  tags: [
+    {
+      name: 'Sign In',
+    },
+    {
+      name: 'Sign Up',
+    },
+  ],
+  paths: {
+    '/signin': signinPath,
+    '/signup': signupPath,
+  },
+  schemas: {
+    account: accountSchema,
+    signupParams: signupParamsSchema,
+    signinParams: signinParamsSchema,
+    error: errorSchema,
+  },
+  components: {
+    badRequest,
+    unauthorized,
+    notFound,
+    serverError,
+  },
+};
---
+++ src/application/rest-api/swagger/paths/signup-path.js
@@ -0,0 +1,42 @@
+export const signupPath = {
+    post: {
+      tags: ['Sign Up'],
+      summary: 'API para criar um usuário',
+      description: 'Essa rota pode ser executada por **qualquer usuário**',
+      requestBody: {
+        required: true,
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/signupParams',
+            },
+          },
+        },
+      },
+      responses: {
+        200: {
+          description: 'Sucesso',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/schemas/signupParams',
+              },
+            },
+          },
+        },
+        400: {
+          $ref: '#/components/badRequest',
+        },
+        401: {
+          $ref: '#/components/unauthorized',
+        },
+        404: {
+          $ref: '#/components/notFound',
+        },
+        500: {
+          $ref: '#/components/serverError',
+        },
+      },
+    },
+  };
+  
